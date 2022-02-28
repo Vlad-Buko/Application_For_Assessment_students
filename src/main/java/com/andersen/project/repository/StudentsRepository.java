a27e4cdecc8016848217485d1dd7d23f5347a0fc
vladislav_domaniewski <vladislav_buko@mail.ru>
2022-02-28 13:32:49 +0300

    Update (0.0.2)* -Update some app

--- src/main/java/com/andersen/project/repository/StudentsRepository.java
+++ src/main/java/com/andersen/project/repository/StudentsRepository.java
@@ -4,5 +4,5 @@ import com.andersen.project.entity.Students;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentsRepository extends JpaRepository<Students, Integer> {
-    Students findByLastName(String lastName);
+
 }
